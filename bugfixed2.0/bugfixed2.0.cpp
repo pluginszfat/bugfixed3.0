bb388bbc8503a051f097e60dd8933017604c7891
pluginszfat <75186847+pluginszfat@users.noreply.github.com>
2021-01-06 20:28:36 +0800

    Add files via upload

---
+++ bugfixed2.0/bugfixed2.0.cpp
@@ -0,0 +1,219 @@
+﻿
+#include<iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <stdlib.h>  
+#include <stdio.h> 
+#include"stdafx.h"
+#include <Windows.h>
+#include <WinInet.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock.h>
+#include <easyx.h>
+#pragma  comment (lib, "ws2_32.lib")
+
+#define ICMP_ECHOREPLY  0  // ICMP回复应答
+#define ICMP_ECHOREQ 8  // ICMP回应请求
+#pragma comment(lib,"wininet.lib")
+
+#define REQ_DATASIZE 32 // 请求数据报大小
+bool internetCheckConnection();
+void showHelp();
+#include <graphics.h>
+#include <conio.h>
+
+
+using namespace std;;
+
+
+
+int main(int argc, char* argv[])
+{
+
+	
+	int choose;
+	int b = rand();
+	cout << "输入 by zfat";
+	cout << endl;
+	cout << "1，自动补全dll库";
+	cout << endl;
+	cout << "2，下载64位JAVA";
+	cout << endl;
+	cout << "3，下载32位JAVA";
+	cout << endl;
+	cout << "4，[cmd版]清除dns缓存";
+	cout << endl;
+	cout << "5，[网卡驱动版]清除dns缓存[需管理权限]";
+	cout << endl;
+	cout << "6，清空[host]文件";
+	cout << endl;
+	cout << "7，网络连接检测1次";
+	cout << endl;
+	cout << "8,[鬼畜版]网络连接检测";
+	cout << endl;
+	cout << "9,获取系统位数,自动下载JAVA";
+	cin >> choose;
+
+	if (choose == 1)
+	{
+		system("start https://visualstudio.microsoft.com/zh-hans/thank-you-downloading-visual-studio/?sku=Community&rel=15#");
+	}
+	if (choose == 2)
+	{
+		system("start https://zfat.lanzous.com/iCKJ5jgj6mh");
+	}
+	if (choose == 3)
+	{
+		system("start https://zfat.lanzous.com/iFuytjgj7of");
+	}
+	if (choose == 4)
+	{
+		system("ipconfig/flushdns");
+		return 0;
+	}
+	if (choose == 5)
+	{
+		cout << endl;
+		cout << "**请以管理员权限运行**";
+		Sleep(1000);
+		cout << endl;
+		cout << "此操作会使您立即断网，确定请输入1";
+		int yes;
+		cin >> yes;
+		if (yes == 1)
+		{
+			
+			system("netsh interface set interface WLAN disabled");
+			cout << "加载中，请稍候";
+			Sleep(5000);
+
+			system("netsh interface set interface WLAN enabled");
+			cout << endl;
+			cout << "ok";
+			Sleep(100000);
+		}
+		else {
+
+			return 0;
+		}
+
+	}
+	if (choose == 6)
+	{
+		cout << "此操作无法恢复,";
+		Sleep(10000);
+		cout << endl;
+		cout << "[警告1]确认删除请按1";
+		int yes;
+		cin >> yes;
+		if (yes == 1)
+		{
+			cout << endl;
+			cout << "[警告2]确认删除请按2";
+			cin >> yes;
+			if (yes == 2)
+			{
+				system("echo.>C:\\Windows\\System32\\drivers\\etc\\hosts");
+				Sleep(100000);
+			}
+			else {
+
+				return 0;
+			}
+		}
+		else {
+
+			return 0;
+		}
+		
+	}
+	if (choose == 7) 
+	{
+		
+	
+    
+        if(internetCheckConnection())
+        {
+            printf("网络正常连接！\n");
+			Sleep(100000);
+            return 0;
+        }
+        else
+        {
+            printf("网络未正常连接！\n");
+			Sleep(100000);
+            return 0;
+        }
+    }
+	if (choose == 8) 
+	{
+		
+		
+		int b= 99;
+		bool ping = false;
+		while (b)
+		{
+			static int count = 0;
+			if (internetCheckConnection())
+			{
+				printf("网络正常连接！\n");
+				Sleep(100000);
+				return 0;
+			}
+			else
+			{
+				if (b != 0)
+				{
+					printf("已检测 %d 次，网络未正常连接！\n", ++count);
+					
+				}
+			}
+		}
+	}
+	if (choose == 9) {
+		SYSTEM_INFO si;
+		GetNativeSystemInfo(&si);
+		if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 || si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_IA64)
+		{
+			
+			cout << "您是64位系统";
+			cout << endl;
+			system("start https://zfat.lanzous.com/iCKJ5jgj6mh");
+		}
+		else {
+			cout << "您是64位系统";
+			cout << endl;
+			system("start https://zfat.lanzous.com/iFuytjgj7of");
+			
+		}
+	}
+else {
+		cout << "听不见大点声 by zfat";
+		return 0;
+	}
+}
+
+bool internetCheckConnection()
+{
+	LPCSTR url[3] = {
+		"http://www.baidu.com/",
+		"http://www.taobao.com/",
+		"http://www.qq.com/"
+	};
+	bool ping = false;
+	int i = 0;
+	while (!ping && i < 3)
+	{
+		ping = InternetCheckConnectionA(url[i], FLAG_ICC_FORCE_CONNECTION, 0);
+		i++;
+	}
+
+	return ping;
+}
+
+
